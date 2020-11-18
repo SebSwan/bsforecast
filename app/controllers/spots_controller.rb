c0cea608d601daa6a481c10740707fcd5a055b67
sebastien etienne <sebastienetienne.pro@gmail.com>
2020-11-18 17:38:59 +0100

    CRUD done

--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -1,15 +1,51 @@
 class SpotsController < ApplicationController
-  def list
+  before_action :set_spot, only: [:show, :destroy, :edit, :update]
+
+  def index
     @spots = Spot.all
   end
 
   def show
-   @spot = Spot.find(params[:id])
   end
 
-  def new; end
+  def new
+    @spot = Spot.new
+  end
+
+  def create
+    @spot = Spot.new(spot_params)
+    @spot.save
+    redirect_to spots_path
+    # show_path(@spot)
+  end
+
+  def edit
+  end
+
+  def update
+    @spot.update(spot_params)
+    redirect_to spots_path
+    # redirect_to show_path(@spot)
+  end
+
+  def destroy
+    @spot.destroy
+
+    redirect_to spots_path
+  end
 
   def wizard; end
 
   def options; end
+
+  private
+
+  def spot_params
+    params.require(:spot).permit(:name, :sport, :configuration, :label, :latitude, :longitude, :wind_force_maxi, :wind_force_mini, :wind_direction, :tide_mini, :tide_max, :low_tide, :mid_tide, :high_tide, :coeff_mini, :coeff_maxi, :wave_direction, :wave_height_mini, :wave_height_maxi, :periode_mini, :periode_maxi, :windfinder, :windfindersuper, :shom )
+  end
+
+  def set_spot
+    @spot = Spot.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,24 @@
 Rails.application.routes.draw do
+
   root to: 'spots#wizard', as: :wizard
-  get 'list', to: 'spots#list', as: :list
-  get 'show/:id', to: 'spots#show', as: :show
   get 'option', to: 'spots#options', as: :options
-  get 'new', to: 'spots#new', as: :new
+
+  resources :spots
+
+  # get 'spots', to: 'spots#index', as: :index
+
+
+  # get 'spots/new', to: 'spots#new', as: :new
+  # post 'spots', to: 'spots#create'
+
+  # get 'spots/show/:id/edit', to: 'spots#edit', as: :edit
+  # patch 'spots/:id', to: 'spots#update', as: :update
+
+  # get 'spots/show/:id', to: 'spots#show', as: :show
+
+  # delete 'spots/:id', to: 'spots#destroy', as: :delete
+
   # For details on the DSL available wizardthin this file, see https://guides.rubyonrails.org/routing.html
 end
+
+
